c87161b18cb4e740b06b5b91484519c6885a575b
gianiaz <gianiaz@gmail.com>
2016-08-26 10:52:34 +0200

    Updates

---
+++ app.347b676bae0245432ba3.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],{0:function(n,e,o){o(1);var t=o(30);document.body.appendChild(t())},30:function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Ciao Mondo",n}}});
+//# sourceMappingURL=app.347b676bae0245432ba3.js.map
---
+++ style.41ef66aba6448ffdfd50.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],{0:function(n,o,c){c(31),n.exports=c(35)},31:function(n,o){},35:function(n,o){}});
+//# sourceMappingURL=style.41ef66aba6448ffdfd50.js.map
